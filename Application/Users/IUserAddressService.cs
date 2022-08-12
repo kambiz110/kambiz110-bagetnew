fb2bcb09d4b15941c22036907726bc39c1d8adee
kami83 <kampergig@gmail.com>
2022-08-12 17:06:27 +0430

    edit adress in customer

--- Application/Users/IUserAddressService.cs
+++ Application/Users/IUserAddressService.cs
@@ -58,7 +58,7 @@ namespace Application.Users
         public string State { get; set; }
         public string ZipCode { get; set; }
         public string PostalAddress { get; set; }
-        public string ReciverName { get; private set; }
+        public string ReciverName { get; set; }
         public string UserId { get; set; }
     }
 }
