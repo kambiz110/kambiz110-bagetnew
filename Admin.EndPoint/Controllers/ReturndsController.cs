6e6c919b58ebee13e173222b3b1d3f91d5db750b
kami83 <kampergig@gmail.com>
2022-09-11 18:55:47 +0430

    یلسیبل

--- Admin.EndPoint/Controllers/ReturndsController.cs
+++ Admin.EndPoint/Controllers/ReturndsController.cs
@@ -1,12 +1,17 @@
-﻿using Application.PostalProducts;
+﻿using Application.Dtos;
+using Application.PostalProducts;
 using Application.PostalProducts.Dto;
 using Application.Returneds.Query;
 using Application.ReturnPaymentInvoice.Commend;
 using Application.ReturnPaymentInvoice.Dto;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,14 +23,15 @@ namespace Admin.EndPoint.Controllers
         private readonly IReturnedForAdminService returnedForAdmin;
         private readonly IAddPostalProductService addPostalProduct;
         private readonly IAddReturnPaymentInvoice returnPaymentInvoice;
-
-        public ReturndsController(IReturnedForAdminService returnedForAdmin, 
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public ReturndsController(IReturnedForAdminService returnedForAdmin,
             IAddPostalProductService addPostalProduct,
-            IAddReturnPaymentInvoice returnPaymentInvoice)
+            IAddReturnPaymentInvoice returnPaymentInvoice, IHostingEnvironment hostingEnvironment)
         {
             this.returnedForAdmin = returnedForAdmin;
             this.addPostalProduct = addPostalProduct;
             this.returnPaymentInvoice = returnPaymentInvoice;
+            _hostingEnvironment = hostingEnvironment;
         }
         public IActionResult Index(int returnStatus = 0)
         {
@@ -36,6 +42,15 @@ namespace Admin.EndPoint.Controllers
         public IActionResult ReturndDetails(int returnedId)
         {
             var model = returnedForAdmin.GetAdminOrderDitales(returnedId);
+            if ((int)model.ReturnedStatus == 1)
+            {
+                var rootPath = _hostingEnvironment.ContentRootPath;
+                var fullPath = Path.Combine(rootPath, "wwwroot/Files/BankDate/UserBankData.json");
+                var jsonData = System.IO.File.ReadAllText(fullPath);
+
+                var item = JsonConvert.DeserializeObject<List<BankDitel>>(jsonData);
+                ViewData["BankOrigins"] = new SelectList(item.Select(p => new { Id = p.BankOriginNumber, Text = p.UserName + "_" + p.BankOrigin }).ToList(), "Id", "Text");
+            }
             return View(model);
         }
         [HttpPost]
@@ -47,8 +62,26 @@ namespace Admin.EndPoint.Controllers
         [HttpPost]
         public async Task<IActionResult> ReturnPayment(AddReturnPaymentInvoiceDto dto)
         {
-             await returnPaymentInvoice.addDataToDb(dto);
-            return RedirectToAction("Index");
+            ModelState.Remove("BankOrigin");
+            if (!ModelState.IsValid)
+            {
+                var query = from state in ModelState.Values
+                            from error in state.Errors
+                            select error.ErrorMessage;
+
+                var errorList = query.ToList();
+                TempData["error"] = errorList;
+                var referer = HttpContext.Request.Headers["Referer"].ToString();
+                return new JsonResult(new ResultDto { IsSuccess = false, Message = "false" });
+            }
+            
+            var rootPath = _hostingEnvironment.ContentRootPath;
+            var fullPath = Path.Combine(rootPath, "wwwroot/Files/BankDate/UserBankData.json");
+            var jsonData = System.IO.File.ReadAllText(fullPath);
+            var item = JsonConvert.DeserializeObject<List<BankDitel>>(jsonData);
+            dto.BankOrigin = item.Where(p => p.BankOriginNumber == dto.BankOriginNumber).FirstOrDefault().BankOrigin;
+            await returnPaymentInvoice.addDataToDb(dto);
+            return new JsonResult(new ResultDto { IsSuccess = true, Message = "موفق" });
         }
     }
 }
--- Application/ReturnPaymentInvoice/Dto/AddReturnPaymentInvoiceDto.cs
+++ Application/ReturnPaymentInvoice/Dto/AddReturnPaymentInvoiceDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,25 @@ namespace Application.ReturnPaymentInvoice.Dto
 {
    public class AddReturnPaymentInvoiceDto
     {
-        public int Amount { get; private set; }
+        public int Amount { get;  set; }
+        [Display(Name = "تاریخ و زمان واریز")]
+        [Required(ErrorMessage = " {0}  را وارد نمائید ")]
         public DateTime DatePay { get; set; }
         public string FollowKey { get; set; }
         public string BankOriginNumber { get; set; }
         public string BankOrigin { get; set; }
+        [Display(Name = "نام بانک مقصد")]
+        [Required(ErrorMessage = " {0}  را وارد نمائید ")]
         public string BankDestination { get; set; }
+        [Display(Name = "شماره حساب بانک مقصد")]
+        [Required(ErrorMessage = " {0}  را وارد نمائید ")]
         public string BankDestinationNumber { get; set; }
         public int ReturnedId { get; set; }
+    }
+    public class BankDitel
+    {
+        public string UserName { get; set; }
+        public string BankOrigin { get; set; }
+        public string BankOriginNumber { get; set; }
     }
 }
