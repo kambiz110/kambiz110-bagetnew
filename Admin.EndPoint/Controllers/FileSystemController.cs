b3d4556d9326a48bf893e0a05f839865754368d0
kambiz <kampergig@gmail.com>
2022-10-03 12:02:24 +0330

    lock image

--- Admin.EndPoint/Controllers/FileSystemController.cs
+++ Admin.EndPoint/Controllers/FileSystemController.cs
@@ -17,18 +17,20 @@ namespace Admin.EndPoint.Controllers
         [Route("connector")]
         public async Task<IActionResult> Connector()
         {
-            var connector = GetConnector();
+            var IsAdmin = User.IsInRole("Administrator");
+            var connector = GetConnector(IsAdmin);
             return await connector.ProcessAsync(Request);
         }
 
         [Route("thumb/{hash}")]
         public async Task<IActionResult> Thumbs(string hash)
         {
-            var connector = GetConnector();
+            var IsAdmin = User.IsInRole("Administrator");
+            var connector = GetConnector(IsAdmin);
             return await connector.GetThumbnailAsync(HttpContext.Request, HttpContext.Response, hash);
         }
 
-        private Connector GetConnector()
+        private Connector GetConnector(bool isadmin)
         {
             var driver = new FileSystemDriver();
 
@@ -42,7 +44,7 @@ namespace Admin.EndPoint.Controllers
             {
                 //IsReadOnly = !User.IsInRole("Administrators")
                 IsReadOnly = false, // Can be readonly according to user's membership permission
-                IsLocked = false, // If locked, files and directories cannot be deleted, renamed or moved
+                IsLocked = !isadmin, 
                 Alias = "مدیریت_فایل_فروشگاه_زنجانی", // Beautiful name given to the root/home folder
                                                        //MaxUploadSizeInKb = 2048, // Limit imposed to user uploaded file <= 2048 KB
                 AccessControlAttributes = new HashSet<NamedAccessControlAttributeSet>()
