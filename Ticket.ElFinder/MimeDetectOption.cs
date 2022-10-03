cb9ebaa3b0175826beec4175b907dbd47d46a686
kambiz <kampergig@gmail.com>
2022-10-03 10:45:41 +0330

    file maneger

---
+++ Ticket.ElFinder/MimeDetectOption.cs
@@ -0,0 +1,12 @@
+﻿namespace Ticket.ElFinder
+{
+    public enum MimeDetectOption : byte
+    {
+        Auto = 0,
+        Internal = 1,
+
+        // Not supported
+        //FInfo = 2,
+        //MimeContentType = 3
+    }
+}
---
+++ Ticket.ElFinder/Models/Commands/Open/OpenResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace Ticket.ElFinder.Models.Commands.Open
+{
+    [DataContract]
+    public class OpenResponse : BaseOpenResponseModel
+    {
+        public OpenResponse(DirectoryModel currentWorkingDirectory, FullPath fullPath) : base(currentWorkingDirectory)
+        {
+            Options = new Options(fullPath);
+            Files.Add(currentWorkingDirectory);
+        }
+    }
+}
