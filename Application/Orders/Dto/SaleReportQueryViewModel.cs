3a9a06d2a0ba51eebca2fd2e7e19bddef0199114
kambiz <kampergig@gmail.com>
2022-12-18 12:00:38 +0330

    sdfsdf

---
+++ Application/Orders/Dto/SaleReportQueryViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Orders.Dto
+{
+    public class FerstSaleReportQueryViewModel
+    {
+        public BuyDto Today { get; set; }
+        public BuyDto YesterDay { get; set; }
+        public BuyDto NowWeek { get; set; }
+        public BuyDto LastWeek { get; set; }
+        public BuyDto NowMonthd { get; set; }
+        public BuyDto LastMonthd { get; set; }
+    }
+    public class ReportQueryViewModel
+    {
+        public BuyReportCountDto Statistics { get; set; }
+        public BuyDto Buyers { get; set; }
+    }
+    public class BuyReportCountDto
+    {
+        public string[] Display { get; set; }
+        public int[] Value { get; set; }
+
+    }
+    public class BuyDto
+    {
+        public int Buyers { get; set; }
+        public int BuyCount { get; set; }
+        public BuyReportCountDto SaleReport { get; set; }
+    }
+}
---
+++ Application/Orders/Query/ISaleReportQuery.cs
@@ -0,0 +1,55 @@
+﻿using Application.Dtos;
+using Application.Interfaces.Contexts;
+using Application.Orders.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Orders.Query
+{
+   public interface ISaleReportQuery
+    {
+      ResultDto<FerstSaleReportQueryViewModel> FirstDefultSaleReport();
+    }
+    public class SaleReportQuery : ISaleReportQuery
+    {
+        private readonly IDataBaseContext _dbContext;
+
+        public SaleReportQuery(IDataBaseContext DbContext)
+        {
+            _dbContext = DbContext;
+        }
+
+        public ResultDto<FerstSaleReportQueryViewModel> FirstDefultSaleReport()
+        {
+            DateTime start = DateTime.Now.Date;
+            DateTime end = DateTime.Now.AddDays(1);
+            //آمار   امروز 
+            var TodayPageViewCount = _dbContext.Orders.AsQueryable()
+    .Where(p => p.ZamanDelivered >= start && p.ZamanDelivered < end).Count();
+
+            //آمار  در 1 روز گذشته 
+            var YesterdayViewCount = _dbContext.Orders.AsQueryable()
+                .Where(p => p.ZamanDelivered >= start.AddDays(-1) && p.ZamanDelivered < end.AddDays(-1)).Count();
+            var TodayVisitorCount = _dbContext.Orders
+                .Where(p => p.ZamanDelivered >= start.AddDays(-1) && p.ZamanDelivered < end.AddDays(-1)).GroupBy(p => p.UserId)
+                .Count();
+            var AllPageViewCount = _dbContext.MonthLogs.Sum(p => p.CountLog);
+           
+            //آمار  در 30 روز گذشته بر اساس کاربر منحصر به فرد
+            var AllVisitorCountByVisitorId = _dbContext.Orders
+                 .Where(p => p.ZamanDelivered >= DateTime.Now.AddDays(-30))
+                .GroupBy(p => p.UserId).Count();
+
+            //آمار  در 30 روز گذشته 
+            var AllVisitorCountBy = _dbContext.Orders
+                 .Where(p => p.ZamanDelivered >= DateTime.Now.AddDays(-30))
+                .Count();
+
+
+            return new ResultDto<FerstSaleReportQueryViewModel>();
+        }
+    }
+}
