3909a3cfd6bb4ba3c1925dffbc1934e77b1332b0
kami83 <kampergig@gmail.com>
2022-12-20 21:44:56 +0330

    buierReport

---
+++ Admin.EndPoint/Helper/Component/GetSaleReportView.cs
@@ -0,0 +1,29 @@
+﻿using Application.Orders.Query;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Admin.EndPoint.Helper.Component
+{
+    public class GetSaleReportView : ViewComponent
+    {
+        private readonly ISaleReportQuery _saleReportQuery;
+
+        public GetSaleReportView(ISaleReportQuery saleReportQuery)
+        {
+            _saleReportQuery = saleReportQuery;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var result = _saleReportQuery.FirstDefultSaleReport();
+            var viewName = string.Format(
+            "~/Views/Shared/Components/{0}.cshtml",
+            this.ViewComponentContext.ViewComponentDescriptor.ShortName);
+            return View(viewName, result.Data);
+        }
+
+    }
+}
--- Application/Orders/Query/ISaleReportQuery.cs
+++ Application/Orders/Query/ISaleReportQuery.cs
@@ -37,50 +37,141 @@ namespace Application.Orders.Query
             .Include(p => p.OrderItems)
             .OrderBy(p => p.OrderDate).ToList();
 
-            var SsaleReportToday= TodayPageView.GroupBy(p=>p.OrderDate.Date);
+            var SsaleReportToday = TodayPageView.GroupBy(p => p.OrderDate.Date);
 
-            model.Today = new BuyDto {
-                BuyCount = TodayPageView.Count(),
-                Buyers = TodayPageView.GroupBy(p => p.UserId).Count(),
-                SaleReport = new BuyReportCountDto {
-                    Value = SsaleReportToday.Select(o => new {
-                        a = o.Select(p => p.OrderItems.Sum(o => o.UnitPrice * o.Units)).Sum(t => t)
-                    }).Select(p => p.a).ToArray(),
-                    Display= SsaleReportToday.Select(p=>p.Key.ToPersianDateStrFarsi()).ToArray(),
-                },
-                 };
-            //آمار  در 1 روز گذشته 
-            var YesterdayView = _dbContext.Orders.AsQueryable()
-                .Where(p => p.OrderDate >= start.AddDays(-1)
-                && p.OrderDate < end.AddDays(-1)
-                && (int)p.PaymentStatus == 1).Include(p => p.OrderItems)
-                       .OrderBy(p => p.OrderDate)
-            .GroupBy(p => p.OrderDate.Date);
-
-            //آمار  در 7 روز گذشته 
-            var SevendayView = _dbContext.Orders.AsQueryable()
-                .Where(p => p.OrderDate >= start.AddDays(-1)
-                && p.OrderDate < end.AddDays(-7)
-                && (int)p.PaymentStatus == 1).Include(p => p.OrderItems)
-                       .OrderBy(p => p.OrderDate)
-            .GroupBy(p => p.OrderDate.Date);
-
-
+            model.Today = GetBuyPerDayDiteles(7);
+            var SevenDay = GetBuyPerDayDitelesWithStartEnd(DateTime.Now.AddDays(-7) , 7);
+            var buycount = GetBuyPerDayCount(7);
+            var BuyPerDay = GetBuyPerDay(7);
 
             //آمار  در 30 روز گذشته بر اساس کاربر منحصر به فرد
-            var AllVisitorCountByVisitorId = _dbContext.Orders
-                 .Where(p => p.OrderDate >= DateTime.Now.AddDays(-30))
-                .GroupBy(p => p.UserId).Count();
+            //var AllVisitorCountByVisitorId = _dbContext.Orders
+            //     .Where(p => p.OrderDate >= DateTime.Now.AddDays(-30))
+            //    .GroupBy(p => p.UserId).Count();
 
-            //آمار  در 30 روز گذشته 
-            var BuyThirtyDay = _dbContext.Orders
-                 .Where(p => p.OrderDate >= DateTime.Now.AddDays(-30)
-                 && (int)p.PaymentStatus == 1)
-                .Include(p => p.OrderItems)
-                   .OrderBy(p => p.OrderDate)
-            .GroupBy(p => p.OrderDate.Date);
+    
 
             return new ResultDto<FerstSaleReportQueryViewModel>();
+        }
+        private BuyReportCountDto GetBuyPerDayCount(int countDay = 30)
+        {
+            DateTime MonthStart = DateTime.Now.Date.AddDays(-(countDay));
+            DateTime MonthEnds = DateTime.Now.Date.AddDays(1);
+            var QueryPageView = _dbContext.Orders.Where(p => (int)p.PaymentStatus == 1).AsQueryable()
+             .Where(p => p.OrderDate >= MonthStart && p.OrderDate < MonthEnds)
+                .Select(p => new { p.OrderDate })
+                .ToList();
+            BuyReportCountDto visitPerDay = new BuyReportCountDto() { Display = new string[countDay], Value = new int[countDay] };
+            for (int i = 0; i < countDay; i++)
+            {
+                var currentday = DateTime.Now.AddDays(i * (-1));
+                var currentFarsiDay = currentday.ToPersianDateString();
+                visitPerDay.Display[i] = currentFarsiDay;
+                visitPerDay.Value[i] = QueryPageView.Where(p => p.OrderDate.Date == currentday.Date).Count();
+            }
+
+            return visitPerDay;
+        }
+        private BuyDto GetBuyPerDayDiteles(int countDay)
+        {
+            DateTime MonthStart = DateTime.Now.Date.AddDays(-(countDay));
+            DateTime MonthEnds = DateTime.Now.Date.AddDays(1);
+          
+            var QueryPageView = _dbContext.Orders.AsQueryable()
+                 .Where(p => (int)p.PaymentStatus == 1 &&
+                 p.OrderDate >= MonthStart &&
+                 p.OrderDate < MonthEnds)
+                .Include(p => p.OrderItems)
+                .OrderBy(p => p.OrderDate).AsQueryable();
+
+            var SsaleReportToday = QueryPageView.GroupBy(p => p.OrderDate.Date);
+            var result = new BuyDto();
+            result.BuyCount = QueryPageView.Count();
+            result.Buyers = QueryPageView.GroupBy(p => p.UserId).Count();
+            BuyReportCountDto visitPerDay = new BuyReportCountDto() { Display = new string[countDay], Value = new int[countDay] };
+            for (int i = 0; i < countDay; i++)
+            {
+                var currentday = DateTime.Now.AddDays(i * (-1));
+                var currentFarsiDay = currentday.ToPersianDateString();
+                visitPerDay.Display[i] = currentFarsiDay;
+                if (QueryPageView.Where(p => p.OrderDate.Date == currentday.Date).Count() > 0)
+                {
+                    visitPerDay.Value[i] = QueryPageView.Where(p => p.OrderDate.Date == currentday.Date)
+                        .Select(p => p.OrderItems.Sum(o => o.UnitPrice * o.Units)).ToList().Sum(p => p);
+                }
+                else
+                {
+                    visitPerDay.Value[i] = QueryPageView.Where(p => p.OrderDate.Date == currentday.Date).Count();
+                }
+
+            }
+            result.SaleReport = visitPerDay;
+            return result;
+        }
+        private BuyReportCountDto GetBuyPerDay(int countDay)
+        {
+            DateTime MonthStart = DateTime.Now.Date.AddDays(-(countDay));
+            DateTime MonthEnds = DateTime.Now.Date.AddDays(1);
+            var QueryPageView = _dbContext.Orders.Where(p=> (int)p.PaymentStatus == 1 ).AsQueryable()
+                .Include(p => p.OrderItems)
+                .Where(p => p.OrderDate >= MonthStart && p.OrderDate < MonthEnds)
+              .AsQueryable();
+            BuyReportCountDto visitPerDay = new BuyReportCountDto() { Display = new string[countDay], Value = new int[countDay] };
+            for (int i = 0; i < countDay; i++)
+            {
+                var currentday = DateTime.Now.AddDays(i * (-1));
+                var currentFarsiDay = currentday.ToPersianDateString();
+                visitPerDay.Display[i] = currentFarsiDay;
+                if (QueryPageView.Where(p => p.OrderDate.Date == currentday.Date).Count() > 0)
+                {
+                    visitPerDay.Value[i] = QueryPageView.Where(p => p.OrderDate.Date == currentday.Date)
+                        .Select(p => p.OrderItems.Sum(o => o.UnitPrice * o.Units)).ToList().Sum(p=>p);
+                }
+                else
+                {
+                    visitPerDay.Value[i] = QueryPageView.Where(p => p.OrderDate.Date == currentday.Date).Count();
+                }
+
+            }
+
+            return visitPerDay;
+        }
+
+        private BuyDto GetBuyPerDayDitelesWithStartEnd(DateTime start, int countDay)
+        {
+            DateTime MonthStart = start;
+            DateTime MonthEnds = start.AddDays(-(countDay));
+
+            var QueryPageView = _dbContext.Orders.AsQueryable()
+                 .Where(p => (int)p.PaymentStatus == 1 &&
+                 p.OrderDate >= MonthStart &&
+                 p.OrderDate < MonthEnds)
+                .Include(p => p.OrderItems)
+                .OrderBy(p => p.OrderDate).AsQueryable();
+
+            var SsaleReportToday = QueryPageView.GroupBy(p => p.OrderDate.Date);
+            var result = new BuyDto();
+            result.BuyCount = QueryPageView.Count();
+            result.Buyers = QueryPageView.GroupBy(p => p.UserId).Count();
+            BuyReportCountDto visitPerDay = new BuyReportCountDto() { Display = new string[countDay], Value = new int[countDay] };
+            for (int i = 0; i < countDay; i++)
+            {
+                var currentday = start.AddDays(i * (-1));
+                var currentFarsiDay = currentday.ToPersianDateString();
+                visitPerDay.Display[i] = currentFarsiDay;
+                if (QueryPageView.Where(p => p.OrderDate.Date == currentday.Date).Count() > 0)
+                {
+                    visitPerDay.Value[i] = QueryPageView.Where(p => p.OrderDate.Date == currentday.Date)
+                        .Select(p => p.OrderItems.Sum(o => o.UnitPrice * o.Units)).ToList().Sum(p => p);
+                }
+                else
+                {
+                    visitPerDay.Value[i] = QueryPageView.Where(p => p.OrderDate.Date == currentday.Date).Count();
+                }
+
+            }
+            result.SaleReport = visitPerDay;
+            return result;
         }
     }
 }
